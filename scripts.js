c5c26028c3977d92abe8b642e2701dd5f1f3e8ab
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-05-10 21:22:19 -0700

    clean up javascript code

--- scripts.js
+++ scripts.js
@@ -1,42 +1,39 @@
-//google map 
+
 let map;
 //track markers to delete
 let markers = [];
+//===============================================
+//INIT MAP
+//===============================================
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.0522, lng: -118.2437},
-    zoom: 8
+    zoom: 12
   });
 }
+
+//===============================================
+//FETCH DATA
+//===============================================
 //FPACT Data
 //cors proxy -- You can easily run your own proxy using code from https://github.com/Rob--W/cors-anywhere/.
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 let url1 = 'http://dhcs-chhsagency.opendata.arcgis.com/datasets/a2742f60dd944a1fa49377bd0e8a7772_0.geojson';
-let testUrl = 'https://opendata.arcgis.com/datasets/299cd32301b34e1792812ce8b5c30fe7_0.geojson'
 fetch(proxyurl + url1)
   .then(function(response) {
     return response.json();
   })
   .then(function(fpactData) {
     console.log(fpactData);
-    //add all markers
-    //addAllMarkers(fpactData);
+    let center = new google.maps.LatLng(34.0522, -118.2437)
+    let radius = 5 * 1609; // convert meters to miles
+    addNearestMarkers(fpactData, center, radius);
 
-    nearestMarkers(fpactData);
-
-
-   //  fpactData.features.forEach(feature=>{
- 		// 	let newPoint = {
- 		// 		lat: feature.geometry.coordinates[1], 
-  	// 		lng: feature.geometry.coordinates[0]
- 		// 	}
- 		// 	let center = {lat: 34.0522, lng: -118.2437}
- 		// 	let otherPoint = {lat: 32.0522, lng: -118.2437}
- 		// 	console.log(google.maps.geometry.spherical.computeDistanceBetween(otherPoint, center));
- 		// })
   });
 
-  //add all markers
+  //===============================================
+  // ADD ALL MARKERS
+  //===============================================
   function addAllMarkers(geojson){
   	geojson.features.forEach(feature=>{
   		let marker = new google.maps.Marker({
@@ -50,30 +47,23 @@ fetch(proxyurl + url1)
   	})
   }
 
-  //store locator
-  // set center
-  // set radius
-  // iterate through lat, lng points
-  //  computeDistanceBetween
-  //   if distance < radius
-  //     set marker
-  //   else 
- 	//     do nothing
-
  	let center = {lat: 34.0522, lng: -118.2437}
  	let radius = 5 * 1609; //meters to miles 
 
- 	function nearestMarkers(geojson){
+
+  //===============================================
+  // ADD NEAREST MARKERS (geojson, center, radius)
+  //geojson - a geojson object
+  //center - a google maps latLng object (eg ...let center = new google.maps.LatLng(34.0522, -118.2437)...)
+  //radius - number (in miles) function converts radius from meters to miles 
+  //===============================================
+ 	function addNearestMarkers(geojson, center, radius){
  		geojson.features.forEach(feature=>{
- 			// let newPoint = {
- 			// 	lat: feature.geometry.coordinates[1], 
-  		// 	lng: feature.geometry.coordinates[0]
- 			// }
  			let newPoint = new google.maps.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
- 			let middle = new google.maps.LatLng(34.0522, -118.2437)
- 			let radius = 5 * 1609; // convert meters to miles
- 			console.log(google.maps.geometry.spherical.computeDistanceBetween(newPoint, middle));
- 			if(google.maps.geometry.spherical.computeDistanceBetween(newPoint, middle)<radius){
+ 			console.log(radius);
+
+ 			//computeDistanceBetween is part of google maps geometry library loaded in html
+ 			if(google.maps.geometry.spherical.computeDistanceBetween(newPoint, center) < radius){
  				//add marker
 	 			let marker = new google.maps.Marker({
 	  			position: {
@@ -99,10 +89,6 @@ fetch(proxyurl + url1)
   //remove all markers 
   //function removeAllMarkers(geoJson)
 
-
-
   //sort by county
-
-  //sort by distance
 
   //add marker labels
