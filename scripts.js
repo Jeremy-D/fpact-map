df5eeaf57bb78755c10a2a7293dc96d09ca2601c
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2019-05-18 09:02:57 -0700

    [style] format extra space at the beginning of scripts.js

--- scripts.js
+++ scripts.js
@@ -58,6 +58,9 @@ function initMap() {
   })
   //end autocomplete function================================================
 }
+//===============================================
+//END INIT MAP
+//===============================================
 
 
 //===============================================
@@ -80,207 +83,210 @@ fetch(url1)
     let radius = 5 * 1609; // convert meters to miles
     addNearestMarkers(data, center, radius);
   });
+//===============================================
+//END FETCH DATA
+//===============================================
 
-  //===============================================
-  // ADD NEAREST MARKERS (geojson, center, radius)
-  //geojson - a geojson object
-  //center - a google maps latLng object (eg ...let center = new google.maps.LatLng(34.0522, -118.2437)...)
-  //radius - number (in miles) function converts radius from meters to miles 
-  //===============================================
- 	function addNearestMarkers(geojson, center, radius){
-    let contentString = '';
-    console.log(geojson);
- 		geojson.features.forEach(feature=>{
- 			let newPoint = new google.maps.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
- 			//computeDistanceBetween is part of google maps geometry library loaded in html
- 			if(google.maps.geometry.spherical.computeDistanceBetween(newPoint, center) < radius){
-        //add infoWindow
-        infoContent = feature.properties;
+//===============================================
+// ADD NEAREST MARKERS (geojson, center, radius)
+//geojson - a geojson object
+//center - a google maps latLng object (eg ...let center = new google.maps.LatLng(34.0522, -118.2437)...)
+//radius - number (in miles) function converts radius from meters to miles 
+//===============================================
+	function addNearestMarkers(geojson, center, radius){
+  let contentString = '';
+  console.log(geojson);
+		geojson.features.forEach(feature=>{
+			let newPoint = new google.maps.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
+			//computeDistanceBetween is part of google maps geometry library loaded in html
+			if(google.maps.geometry.spherical.computeDistanceBetween(newPoint, center) < radius){
+      //add infoWindow
+      infoContent = feature.properties;
 
-        //place request
-        //let address = infoContent
+      //place request
+      //let address = infoContent
 
 
-        contentString = `<div class=info-window-container>
-      <h1>`+ infoContent.Provider_Businness_Legal_Name +`</h1>
-      <p>Alias` + infoContent.Provider_Address_Attention_Line + `</p>
-      <p>City: ` + infoContent.Provider_Address_City +`</p>
-      <p>County: ` + infoContent.Provider_Address_County_Code_De +`</p>
-      <p>Zip: ` + infoContent.Provider_Address_Zip +` </p>
-      <p>Phone: ` + 'phone number needed, google maps places API?' +` </p>
-      <p>Type of Center: ` + infoContent.Provider_Type_Code_Desc +`</p>
-    </div>`
- 				//add marker
-        addMarker(feature.geometry.coordinates[1], feature.geometry.coordinates[0], contentString)
- 			}
- 		})
- 	}
+      contentString = `<div class=info-window-container>
+    <h1>`+ infoContent.Provider_Businness_Legal_Name +`</h1>
+    <p>Alias` + infoContent.Provider_Address_Attention_Line + `</p>
+    <p>City: ` + infoContent.Provider_Address_City +`</p>
+    <p>County: ` + infoContent.Provider_Address_County_Code_De +`</p>
+    <p>Zip: ` + infoContent.Provider_Address_Zip +` </p>
+    <p>Phone: ` + 'phone number needed, google maps places API?' +` </p>
+    <p>Type of Center: ` + infoContent.Provider_Type_Code_Desc +`</p>
+  </div>`
+				//add marker
+      addMarker(feature.geometry.coordinates[1], feature.geometry.coordinates[0], contentString)
+			}
+		})
+	}
 
-  //===============================================
-  // CLEAR MAP
-  //===============================================
-  // Sets the map on all markers in the array.
-  function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-    }
+//===============================================
+// CLEAR MAP
+//===============================================
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
   }
+}
 
-  // Removes the markers from the map, but keeps them in the array.
-  // The function can be used to toggle markers.
-  function clearMarkers() {
-    setMapOnAll(null);
-  }
+// Removes the markers from the map, but keeps them in the array.
+// The function can be used to toggle markers.
+function clearMarkers() {
+  setMapOnAll(null);
+}
 
-  // Deletes all markers in the array by removing references to them.
-  function deleteMarkers() {
-    clearMarkers();
-    markers = [];
-  }
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
+//===============================================
+// ADD MARKER
+//===============================================
+function addMarker(lat, lng, contentString){
+  let marker = new google.maps.Marker({
+    position: {
+      lat: lat, 
+      lng: lng
+    },
+    map: map
+  })
+  //infoWindow param might break addAllMarkers function
+
+  let infowindow = new google.maps.InfoWindow({
+    content: contentString
+  })
+  console.log(marker)
+  console.log(infowindow)
+  addInfoWindowToMarker(marker, infowindow);
+  markers.push(marker)
+}
+
+//===============================================
+// address query test
+//-- get's the phone number
+//figure out async await stuff
+//===============================================
+let testAddress = 'HOLLYWOOD WILSHIRE HLTH';
+//let testAddress2 = 'PLANNED PARENTHOOD ASSC, SAN JOSE, CA, 95126';
+//works! add to markers
+
+let superTest;
+
+
+function getPlacesData(address){
+  let request = {
+    query: address,
+    fields: ['name', 'geometry', 'place_id']
+  };
   
-  //===============================================
-  // ADD MARKER
-  //===============================================
-  function addMarker(lat, lng, contentString){
-    let marker = new google.maps.Marker({
-      position: {
-        lat: lat, 
-        lng: lng
-      },
-      map: map
-    })
-    //infoWindow param might break addAllMarkers function
+  let service = new google.maps.places.PlacesService(map);
 
-    let infowindow = new google.maps.InfoWindow({
-      content: contentString
-    })
-    console.log(marker)
-    console.log(infowindow)
-    addInfoWindowToMarker(marker, infowindow);
-    markers.push(marker)
+  let newPlace;
+
+  //async func 1 - getPlaceID()
+  let details = service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      console.log(results[0].place_id)
+      let requestDetails = {
+        placeId: results[0].place_id,
+        fields: ['name', 'formatted_address', 'formatted_phone_number', 'place_id', 'geometry']
+      }
+
+      //async func 2 - getDetails()
+      let level2Data = service.getDetails(requestDetails, function(place, status){
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          //marker must be added at this point to get async data like phone number from Places API
+          superTest = place;
+          console.log('superTest');
+          console.log(superTest);
+          console.log('place');
+          console.log(place);
+          return place
+        }
+      })
+    } else {
+        //return "sorry we can't seem to find that location"
+        console.log("sorry we can't seem to find that location")
+      }
+    //return level2Data
+    //return newPlace;
+    });
+  console.log(details);
+  return details
   }
 
-  //===============================================
-  // address query test
-  //-- get's the phone number
-  //figure out async await stuff
-  //===============================================
-  let testAddress = 'HOLLYWOOD WILSHIRE HLTH';
-  //let testAddress2 = 'PLANNED PARENTHOOD ASSC, SAN JOSE, CA, 95126';
-  //works! add to markers
+  //--private to getPlacesData()=================================================
 
-  let superTest;
-
-
-  function getPlacesData(address){
+  //================== 
+  //getPlaceID() takes an address and returns a place ID
+  //================== 
+  function getPlaceID(address, service){
     let request = {
       query: address,
       fields: ['name', 'geometry', 'place_id']
     };
-    
-    let service = new google.maps.places.PlacesService(map);
+    //Initiate Google PlacesService
+    let placeService = new google.maps.places.PlacesService(map);
+    //findPlaceFromQuery() call
 
-    let newPlace;
+    let details1 = placeQueryAsync(request, placeService, pQAHelper);
 
-    //async func 1 - getPlaceID()
-    let details = service.findPlaceFromQuery(request, function(results, status) {
+    console.log(details1);
+    return details1;
+  }
+
+  //================== 
+  //placeQueryAsync(){}
+  //service.findPlaceFromQuery() needs to be wrapped in a function
+  //that returns a promise
+  //================== 
+  function placeQueryAsync(request, service, fn){
+    service.findPlaceFromQuery(request, function(results, status) {   
       if (status === google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results[0].place_id)
-        let requestDetails = {
-          placeId: results[0].place_id,
-          fields: ['name', 'formatted_address', 'formatted_phone_number', 'place_id', 'geometry']
-        }
-
-        //async func 2 - getDetails()
-        let level2Data = service.getDetails(requestDetails, function(place, status){
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            //marker must be added at this point to get async data like phone number from Places API
-            superTest = place;
-            console.log('superTest');
-            console.log(superTest);
-            console.log('place');
-            console.log(place);
-            return place
-          }
-        })
+        fn(results[0].place_id)
       } else {
-          //return "sorry we can't seem to find that location"
           console.log("sorry we can't seem to find that location")
         }
-      //return level2Data
-      //return newPlace;
-      });
-    console.log(details);
-    return details
-    }
+      })
+  };
 
-    //--private to getPlacesData()=================================================
+  let pleaseWork;
 
-    //================== 
-    //getPlaceID() takes an address and returns a place ID
-    //================== 
-    function getPlaceID(address, service){
-      let request = {
-        query: address,
-        fields: ['name', 'geometry', 'place_id']
-      };
-      //Initiate Google PlacesService
-      let placeService = new google.maps.places.PlacesService(map);
-      //findPlaceFromQuery() call
-
-      let details1 = placeQueryAsync(request, placeService, pQAHelper);
-
-      console.log(details1);
-      return details1;
-    }
-
-    //================== 
-    //placeQueryAsync(){}
-    //service.findPlaceFromQuery() needs to be wrapped in a function
-    //that returns a promise
-    //================== 
-    function placeQueryAsync(request, service, fn){
-      service.findPlaceFromQuery(request, function(results, status) {   
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          fn(results[0].place_id)
-        } else {
-            console.log("sorry we can't seem to find that location")
-          }
-        })
-    };
-
-    let pleaseWork;
-
-    function pQAHelper(code){
-      pleaseWork = code;
-    }
-
-
-
-
-  //===============================================
-  // addInfoWindowToMarker()
-  //===============================================
-  function addInfoWindowToMarker(marker, infowindow, content){
-      marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
+  function pQAHelper(code){
+    pleaseWork = code;
   }
 
 
-  //===============================================
-  // metersToMiles()
-  // google maps distances are in meters by default
-  //===============================================
+
+
+//===============================================
+// addInfoWindowToMarker()
+//===============================================
+function addInfoWindowToMarker(marker, infowindow, content){
+    marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+
+//===============================================
+// metersToMiles()
+// google maps distances are in meters by default
+//===============================================
 
 
 
 
 
 
-  //remove all markers 
-  //function removeAllMarkers(geoJson)
+//remove all markers 
+//function removeAllMarkers(geoJson)
 
-  //sort by county
+//sort by county
 
   //add marker labels
